ecbba6edd487dad460d6917b9fb7224ff41e6f59
EvangelieAnagnostopoulou <evangelie_5@hotmail.com>
2021-10-19 19:10:03 +0300

    Logout kicks user out of keyrock

--- project/views.py
+++ project/views.py
@@ -1,10 +1,20 @@
+from allauth.socialaccount.models import SocialApp
 from django.contrib.auth import logout as logout_fn
-from django.shortcuts import render, redirect
-from django.urls import reverse
+from django.shortcuts import redirect
+
+
+from project.settings import OAUTH_SERVER_BASEURL
 
 
 def logout(request):
     # clear session
     logout_fn(request)
 
-    return redirect(reverse('login'))
+    # get client id
+    client_id = ""
+    app = SocialApp.objects.filter(provider="keyrockprovider").first()
+    if app:
+        client_id = f"&client_id={app.client_id}"
+
+    # logout from keyrock
+    return redirect(f"{OAUTH_SERVER_BASEURL}/auth/external_logout?_method=DELETE{client_id}")
