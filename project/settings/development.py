9d18ae3440a37db7b43cbcd41364f2341c6d5cdb
Fotis Paraskevopoulos <fotis@existanze.com>
2021-10-26 14:44:45 +0300

    Setting database url

--- project/settings/development.py
+++ project/settings/development.py
@@ -1,15 +1,6 @@
 from project.settings.base import *
 
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DATABASE_NAME', 'naiades_db_new'),
-        'USER': os.getenv('DATABASE_USER', 'fotisp'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD', '1234'),
-        'HOST': os.getenv('DATABASE_HOST', 'localhost'),
-        'PORT': os.getenv('DATABASE_PORT', '5432'),
-    }
-}
+import dj_database_url
+# DATABASES['default'] =  dj_database_url.config()
+#updated
+DATABASES = {'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))}
